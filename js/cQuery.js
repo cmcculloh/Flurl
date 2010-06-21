9f25f9f3a19933798505bfc5cec8059c01a34a8d
Christopher McCulloh <cmcculloh@gmail.com>
2010-06-21 12:45:14 -0400

    separated DOM Selection into its own method off of the cQuery object. Separated html into its own method off of the cQuery object, kept reference for chaining. Added shell of AJAX function to cQuery and tested passing an annonymous function through the success parameter to see how it would handle attempted execution. TODO: add check to test for success function existance before attempting to execute it.

--- js/cQuery.js
+++ js/cQuery.js
@@ -10,60 +10,74 @@
  * 
  */
 var cQuery = function(elm){
-	var firstCharacter, body, elements;
-	
 	//if the user didn't pass through an element to be selected, they are chaining or just trying to use a function, so don't re-select
-	if(elm != undefined){	
-		cQuery.DOMElements = [];//initialize empty array for selected DOM elements
-	
-		body = document.getElementsByTagName("body")[0];
-		
-		firstCharacter = elm.charAt(0);
-	
-		//Determine if we are trying to select a single element by ID, multiple elements by classname, or multiple elements by tag name
-		if(firstCharacter === "#"){
-			//select by ID
-			
-			//remove the "#" from the DOM Selector
-			elm = elm.substr(1);
-			DOMElement = document.getElementById(elm);
-			cQuery.DOMElements = [DOMElement];
-		}else if(firstCharacter === "."){
-			//select by className
-			
-			elm = elm.substr(1);		
-			elements = body.getElementsByTagName("*");
-			for(var i = 0; i < elements.length; i++){
-				if(elements[i].className === elm){
-					cQuery.DOMElements.push(elements[i]);
-				}
-			}
-		}else{
-			//select by tagName
-			elements = body.getElementsByTagName(elm);
-			cQuery.DOMElements = elements;
-		}
+	if(elm != undefined){
+		cQuery.DOMSelection(elm);
 	}
 	
+	//enable chaining by adding shortcut references to other cQuery functions
 	return {
 		html:function(newHTML){
-			var theHTML;
-			
-			if(newHTML === undefined){
-				for(var i = 0; i < cQuery.DOMElements.length; i++){
-					theHTML = cQuery.DOMElements[i].innerHTML;
-				}
-				return theHTML;
-			}else{
-				for(var i = 0; i < cQuery.DOMElements.length; i++){
-					cQuery.DOMElements[i].innerHTML = newHTML;
-				}
-
-				return cQuery();
-			}
+			return cQuery.html(newHTML);
 		}
 	};
 };
+
+cQuery.DOMSelection = function(elm){
+	var firstCharacter, body, elements;
+	
+	cQuery.DOMElements = [];//initialize empty array for selected DOM elements
+
+	body = document.getElementsByTagName("body")[0];
+	
+	firstCharacter = elm.charAt(0);
+
+	//Determine if we are trying to select a single element by ID, multiple elements by classname, or multiple elements by tag name
+	if(firstCharacter === "#"){
+		//select by ID
+		
+		//remove the "#" from the DOM Selector
+		elm = elm.substr(1);
+		DOMElement = document.getElementById(elm);
+		cQuery.DOMElements = [DOMElement];
+	}else if(firstCharacter === "."){
+		//select by className
+		
+		elm = elm.substr(1);		
+		elements = body.getElementsByTagName("*");
+		for(var i = 0; i < elements.length; i++){
+			if(elements[i].className === elm){
+				cQuery.DOMElements.push(elements[i]);
+			}
+		}
+	}else{
+		//select by tagName
+		elements = body.getElementsByTagName(elm);
+		cQuery.DOMElements = elements;
+	}	
+}
+
+cQuery.html = function(newHTML){
+	var theHTML;
+	
+	if(newHTML === undefined){
+		for(var i = 0; i < cQuery.DOMElements.length; i++){
+			theHTML = cQuery.DOMElements[i].innerHTML;
+		}
+		return theHTML;
+	}else{
+		for(var i = 0; i < cQuery.DOMElements.length; i++){
+			cQuery.DOMElements[i].innerHTML = newHTML;
+		}
+
+		return cQuery();
+	}
+}
+
+cQuery.ajax = function(url, data, success, error){
+	alert("doing AJAX");
+	success();
+}
 
 //make sure our library is exposed to the global namespace and make a shortcut "_" so we don't have to type cQuery every time.
 window.cQuery = window._ = cQuery;
