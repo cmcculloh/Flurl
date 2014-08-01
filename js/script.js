9ce09afb3b787b9cc6c437d46ae882338cc81f73
Christopher McCulloh <cmcculloh@gmail.com>
2014-08-01 12:59:20 -0400

    (master) trashed flickr photo-stream, because it just didn't work anymore and I don't care

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
-var getPhotos = function(numberOfPhotos){
-	_.ajax("http://chomperstomp.com/flurl/flickr/getPhotos.php",
-		{"numberOfPhotos":numberOfPhotos},
-		function(data){
-			var height, screenHeight, theHTML;
+// var getPhotos = function(numberOfPhotos){
+// 	_.ajax("http://blog.chomperstomp.com/Flurl/flickr/getPhotos.php",
+// 		{"numberOfPhotos":numberOfPhotos},
+// 		function(data){
+// 			var height, screenHeight, theHTML;
 
-			//clear the animation queue TODO: write some logic into the library to allow removal, or replacement, of single animations from queue automatically
-			cQuery.animations = [];
+// 			//clear the animation queue TODO: write some logic into the library to allow removal, or replacement, of single animations from queue automatically
+// 			cQuery.animations = [];
 
-			//since it takes 30 seconds for this to run, call it again immediately
-			setTimeout("getPhotos(5);", 10000);
+// 			//since it takes 30 seconds for this to run, call it again immediately
+// 			setTimeout("getPhotos(5);", 10000);
 
 
-			data = JSON.parse(data);
+// 			data = JSON.parse(data);
 
-			theHTML = "";
-			for(var i = 0; i < data.length; i++){
-				var photo = data[i];
-				//build the HTML
-				theHTML += ['<a href="' + photo['shortUrl'] + '" class="photo">',
-								'<img alt="' + photo['title'] + '" title="' + photo['title'] + '" src="' + photo['imgSrc'] + '">',
-							'</a>'].join('');				
-			}
+// 			theHTML = "";
+// 			for(var i = 0; i < data.length; i++){
+// 				var photo = data[i];
+// 				//build the HTML
+// 				theHTML += ['<a href="' + photo['shortUrl'] + '" class="photo">',
+// 								'<img alt="' + photo['title'] + '" title="' + photo['title'] + '" src="' + photo['imgSrc'] + '">',
+// 							'</a>'].join('');				
+// 			}
 
-			_("#pandaPhotos").html(theHTML);
+// 			_("#pandaPhotos").html(theHTML);
 
-			height = _("#pandaPhotos").height();
-			screenHeight = _("#BGContainer").height();
+// 			height = _("#pandaPhotos").height();
+// 			screenHeight = _("#BGContainer").height();
 
-			_("#pandaPhotos").bottom(0).scrollDown(0, (screenHeight + height), 5, "forever");
-		}
-	);	
-}
+// 			_("#pandaPhotos").bottom(0).scrollDown(0, (screenHeight + height), 5, "forever");
+// 		}
+// 	);	
+// }
 
-getPhotos(5);
+// getPhotos(5);
